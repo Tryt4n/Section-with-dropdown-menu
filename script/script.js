2119a48cde2c1e46e810a2cb0627090b69ebb089
Tryt4n <78303570+Tryt4n@users.noreply.github.com>
2023-01-27 11:39:50 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-import { slideMobileList, handleDefaultStates } from "./navbarSection.js";
+import { slideMobileList, handleDefaultStates } from "./navbar.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   handleDefaultStates();
